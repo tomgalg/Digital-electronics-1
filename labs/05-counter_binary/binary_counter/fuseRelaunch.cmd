f52b9ebd48774187bab7c6d03fc4c6305ea2c720
lab661 <lab661@PC-446.urel.feec.vutbr.cz>
2020-03-04 14:59:56 +0100

    05

---
+++ labs/05-counter_binary/binary_counter/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xgalga00/Digital-electronics-1/labs/05-counter_binary/binary_counter/tb2_isim_beh.exe" -prj "/home/lab661/Documents/xgalga00/Digital-electronics-1/labs/05-counter_binary/binary_counter/tb2_beh.prj" "work.tb2" 
